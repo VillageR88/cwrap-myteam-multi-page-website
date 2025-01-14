a0a2aa35575d3aabf8bd756c060e41f3a96622bc
VillageR88 <villager88pl@gmail.com>
2025-01-14 14:25:21 +0100

    Added transition and initial script
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

---
+++ static/scripts/about.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.documentElement.style.setProperty("--dynamic-time-one", "0.3s");
+    document.documentElement.style.setProperty("--dynamic-time-two", "0.38s");
+  }, 300);
+};
